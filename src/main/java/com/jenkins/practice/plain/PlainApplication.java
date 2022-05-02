ed4a139e1d411cefc3d8fdf6078010522184b5ce
shashi.ranjan <shashi.ranjan@rakuten.com>
2022-05-03 04:42:47 +0530

    added src

---
+++ src/main/java/com/jenkins/practice/plain/PlainApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.practice.plain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlainApplication.class, args);
+	}
+
+}
