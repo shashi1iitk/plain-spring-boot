9e92d4d9ee45d3138f06f8607ff76b30cbe17933
Shashi Ranjan <shashi1iitk@gmail.com>
2022-05-03 08:36:25 +0530

    Update PlainApplicationTests.java

--- src/test/java/com/jenkins/practice/plain/PlainApplicationTests.java
+++ src/test/java/com/jenkins/practice/plain/PlainApplicationTests.java
@@ -3,12 +3,14 @@ package com.jenkins.practice.plain;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class PlainApplicationTests {
 
 	@Test
 	public void assetTest() {
-		assertTrue(True);
+		assertTrue(true);
 	}
 
 }
